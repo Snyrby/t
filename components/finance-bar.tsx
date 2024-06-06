c5b6401c217c2d0d0a950dc594a1190b64e9b7e6
Shawn Ruby <snyrby@gmail.com>
2024-06-06 07:22:05 -0700

    Small progress

--- components/finance-bar.tsx
+++ components/finance-bar.tsx
@@ -1,29 +1,23 @@
 import { cn } from "@/lib/utils";
 
 type FinaceBarProps = {
-    targetRef: React.RefObject<HTMLDivElement>
-    
-}
+  targetRef: React.RefObject<HTMLDivElement>;
+};
 
-export const FinanceBar = ({
-    targetRef,
-}: FinaceBarProps) => {
+export const FinanceBar = ({ targetRef }: FinaceBarProps) => {
   return (
-      <div
-        ref={targetRef}
-        className="h-[50px] bg-[#cc0000]"
-      >
-        <div className="flex justify-between mx-auto my-0">
-          <div className="ml-6 h-full items-center justify-start">
-            Location
-            Store
-          </div>
-          <div className="mr-4">
-            Target Circle
-            Target Circle Card
-            Target Circle 360
-          </div>
+    <div ref={targetRef} className="h-[50px] target-red w-full">
+      <div className="flex justify-between items-center mx-auto my-0 h-full max-w-[1400px]">
+        <div className="h-full flex items-center justify-start gap-x-4">
+          <div className="hover:cursor-pointer">Location</div>
+          <div className="hover:cursor-pointer">Store</div>
+        </div>
+        <div className="mr-4 flex">
+          <div className="">Target Circle</div>
+          <div className="">Target Circle Card</div>
+          <div className="">Target Circle 360</div>
         </div>
       </div>
+    </div>
   );
 };
