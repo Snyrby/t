9fb50386669028d8c3d333d31dd7c562f1be3959
Snyrby <snyrby@gmail.com>
2024-06-14 16:28:13 -0700

    Worked on Modal

--- components/modals/location-modal.tsx
+++ components/modals/location-modal.tsx
@@ -1,10 +1,17 @@
 import { useModal } from "@/hooks/use-modal-store";
-import { on } from "events";
+import clsx from "clsx";
 import { X } from "lucide-react";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+type LocationFormProps = {
+  zipCode: number;
+};
 
 export const LocationModal = () => {
   const { isOpen, onClose, onOpen, type } = useModal();
+  const { register, handleSubmit, setValue, getValues, formState } =
+    useForm<LocationFormProps>();
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Escape" || event.keyCode === 27) {
@@ -19,29 +26,91 @@ export const LocationModal = () => {
   if (type !== "LOCATION" || isOpen === false) {
     return null;
   }
+
+  const isLoading = formState.isSubmitting;
+
+  const onSubmit = async (data: LocationFormProps) => {
+    // await fetch("http://ip-api.com/json")
+    //   .then((res) => {
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     return console.log(data);
+    //   });
+    console.log(data);
+  };
   return (
     <>
       <aside className="fixed top-0 left-0 z-40 h-full bg-white w-96 shadow-lg transform transition-transform duration-300">
-        <div className="flex flex-col ml-3 mr-6">
-          <div className="flex my-3 justify-between">
-            <p className="text-lg">Update shipping location</p>
-            <div className="bg-gray-200 opacity-50 rounded-full transition hover:opacity-100 size-8 flex justify-center items-center">
-              <X onClick={onClose} className="cursor-pointer" size={28} />
-            </div>  
+        <div className="flex flex-col h-full">
+          <div className="flex my-3 justify-between items-center mx-5">
+            <p className="text-xl font-bold">Update shipping location</p>
+            <div className="bg-gray-100 opacity-50 rounded-full transition hover:opacity-100 size-7 flex justify-center items-center">
+              <X
+                onClick={() => onClose()}
+                className="cursor-pointer opacity-100"
+                size={28}
+                color="black"
+              />
+            </div>
           </div>
-          <div className="border" />
-          <p className="text-sm my-4">Item availability and shipping options will change based on location.</p>
-          <div className="relative p-[10px]">
-            <label htmlFor="zipcode" className="absolute top-[-1px] left-5">
-              Zip code
-            </label>
-            <input type="text" name="zipcode" id="zipcode" className="border-[1px] border-black rounded-sm"/>
+          <div className="border mx-0" />
+          <div className="flex flex-col justify-between h-full mx-4">
+            <div className="">
+            <p className="text-sm my-4">
+              Item availability and shipping options will change based on
+              location.
+            </p>
+            <form
+              className="relative"
+              onSubmit={handleSubmit(onSubmit)}
+              noValidate
+            >
+              <label
+                htmlFor="zipCode"
+                className={clsx(
+                  "absolute -top-2 left-3 bg-white text-xs z-10",
+                  formState.errors.zipCode?.message &&
+                    "text-red-600 bg-amber-100"
+                )}
+              >
+                Zip code
+              </label>
+              <input
+                type="text"
+                id="zipCode"
+                {...register("zipCode", {
+                  required: {
+                    value: true,
+                    message: "Zipcode is required",
+                  },
+                  pattern: {
+                    value: /^[0-9]{5}$/,
+                    message: "Invalid zipcode",
+                  },
+                })}
+                className={clsx(
+                  "p-[10px] border-[1px] border-[rgb(136, 136, 136)] rounded-sm w-full outline-none focus:bg-white focus:border-[2px]",
+                  formState.errors.zipCode?.message &&
+                    "border-red-600 bg-amber-100 focus:bg-amber-100"
+                )}
+                disabled={isLoading}
+              />
+            </form>
+            <p className="text-xs text-red-600">
+              {formState.errors.zipCode?.message}
+            </p>
+          </div>
+          <div className="">
+            button
+          </div>
           </div>
         </div>
-        <div />
       </aside>
-      <button className="bg-black fixed inset-0 bg-opacity-50 backdrop-blur-sm z-30" onClick={() => onClose()} />
+      <button
+        className="bg-black fixed inset-0 bg-opacity-50 backdrop-blur-sm z-30"
+        onClick={() => onClose()}
+      />
     </>
   );
 };
-
