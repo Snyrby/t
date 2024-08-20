1951359939cc780c2b7096423fa915d6352a716c
Shawn Ruby <snyrby@gmail.com>
2024-08-20 16:25:21 -0700

    Working on search bar

--- components/ui/search-bar.tsx
+++ components/ui/search-bar.tsx
@@ -2,8 +2,15 @@ import { Mic, Search } from "lucide-react";
 
 export const SearchBar = () => {
   return (
-    <div className="w-auto flexBetween">
-      <p className="text-base tracking-tight">What can we help you find?</p>
+    <div className="w-96 flexBetween rounded-lg h-10 bg-gray-200/30">
+      <input
+        type="search"
+        className="w-full px-4 py-2 focus:outline-none bg-transparent"
+        placeholder="Search..."
+        autoComplete="off"
+        autoCorrect="off"
+        autoCapitalize="off"
+      />
       <div className="flex">
         <Mic size={20} className="text-gray-400" />
         <Search size={20} className="text-gray-400" />
