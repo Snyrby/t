8914a2f899953a124476c4fcb5fa1b0a75291cb1
Shawn Ruby <snyrby@gmail.com>
2024-09-20 07:21:18 -0700

    Starting login/register form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,18 +4,21 @@ import { AuthForm } from "@/components/auth/auth-form";
 import { AuthHeader } from "@/components/auth/auth-header";
 import { AuthLegal } from "@/components/auth/auth-legal";
 import { useModal } from "@/hooks/use-modal-store";
+import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 
 export default function LoginPage() {
   const { isOpen, onClose } = useModal();
+  const searchParams = useSearchParams();
+  const action = searchParams.get("actions");
 
   useEffect(() => {
     isOpen && onClose();
   }, [])
 
   return (
-    <div className="flex flex-col justify-start items-center w-full h-full">
-      <AuthHeader />
+    <div className="flex flex-col justify-start items-center w-full h-full mt-8">
+      <AuthHeader action={action}/>
       <AuthForm />
       <AuthLegal />
       <AuthFooter />
--- components/auth/auth-header.tsx
+++ components/auth/auth-header.tsx
@@ -1,15 +1,38 @@
 import Image from "next/image";
+import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
-export const AuthHeader = () => {
+type AuthHeaderProps = {
+  action: string | null;
+};
+
+type Variant = "LOGIN" | "REGISTER";
+
+export const AuthHeader = ({ action }: AuthHeaderProps) => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      firstName: "",
+      lastNameName: "",
+      email: "",
+      password: "",
+      mobileNumber: "",
+    },
+  });
   return (
     <div className="flex flex-col justify-start items-center">
       <Image
         src="/Target_Bullseye-Logo_Red.jpg"
         alt="target"
-        width={40}
-        height={40}
+        width={35}
+        height={35}
       />
-      <h1 className="">Sign into your Target account</h1>
+      <h1 className="">{action === "create_session_signin" ? "Sign into your Target account" : "Create your Target account"}</h1>
     </div>
   );
 };
